45f3f79e1864676ec95f7a3f3077d4e3c9b32487
WK WK <witold.kepczynski@gmail.com>
2022-06-11 23:22:15 +0200

    added error handling example

--- ErrorHandling/Program.cs
+++ ErrorHandling/Program.cs
@@ -1,9 +1,44 @@
-﻿
+﻿using System.Net.Http;
+
 namespace ErrorHandling;
 
 public static class Program
 {
-    public static void Main(string[] args){
-        System.Console.WriteLine("Error handling in C#");
+    public static void Main(string[] args)
+    {
+
+        var variable = "1";
+
+        try
+        {
+            var converted = int.Parse(variable);
+
+            Console.WriteLine("Hurray I can add 1 to {0} and get {1}\n", converted, converted + 1);
+        }
+        catch (System.FormatException ex)
+        {
+            Console.WriteLine("cannot conver error: {0}\n", ex.Message);
+
+            Environment.Exit(1);
+        }
+        try
+        {
+            var client = new HttpClient();
+
+            var response = client.GetAsync("http://worldtimeapi.org/api/ip").Result;
+
+            response.EnsureSuccessStatusCode();
+
+            var body = response.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine("Current time here and now is {0}\n", body);
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine("cannot get data error {0}", ex.Message);
+
+            Environment.Exit(1);
+        }
+
     }
 }
--- ErrorHandling/main.go
+++ ErrorHandling/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
 
 func main() {
-	fmt.Printf("Hello")
+
+	variable := "1"
+
+	converted, errParse := strconv.Atoi(variable)
+
+	if errParse != nil {
+		log.Fatalf("cannot conver error: %s\n", errParse.Error())
+	}
+
+	log.Printf("Hurray I can add 1 to %d and get %d\n", converted, converted+1)
+
+	response, errHttp := http.Get("http://worldtimeapi.org/api/ip")
+
+	if errHttp != nil {
+		log.Fatalf("cannot get data error: %s\n", errHttp.Error())
+	}
+
+	body, errDecoding := ioutil.ReadAll(response.Body)
+
+	if errDecoding != nil {
+		log.Fatalf("cannot decode error %s\n", errDecoding.Error())
+	}
+
+	log.Printf("Current time here and now is %s\n", body)
 }
